9fd42c5ecb56f585b0c415fbab4dff32d61dd89f
Eric Ren MacBook <dellnoantechnp@gmail.com>
2021-08-11 21:55:48 +0800

    fix commit problem.

---
+++ account-email/src/main/java/com/juvenxu/mvnbook/account/email/AccountEmailException.java
@@ -0,0 +1,7 @@
+package com.juvenxu.mvnbook.account.email;
+
+public class AccountEmailException extends Exception {
+    public AccountEmailException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
